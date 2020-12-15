d50500ad48f32439d85579cd84c276af09fc83ed
Baudouindlv <71937583+Baudouindlv@users.noreply.github.com>
2020-12-15 20:42:04 +0100

    Update Partie.java

---
+++ Partie.java
@@ -0,0 +1,50 @@
+package projet;
+import java.util.*;
+
+public class Partie{
+	private Integer nbrDeJoueur;
+	private Carte maCarte;
+	private ArrayList<Joueur> joueurs;
+	
+	
+	public Partie() {
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Partie [nbrDeJoueur=" + nbrDeJoueur + ", maCarte=" + maCarte + ", joueurs=" + joueurs + "]";
+	}
+	
+	public void lancerPartie() {
+		
+	}
+	
+	public void fermerPartie() {
+		
+	}
+	
+	public void afficherPartie() {
+		// matrice
+	}
+	
+	public void choisirJoueur() {
+		
+	}
+	
+	public ArrayList<Integer> lancerDes(Integer nbr){
+		return null;
+	}
+	
+	public Integer score(ArrayList<Integer> des) {
+		return null;
+	}
+	
+	public void attribuerRenfort(String idJoueur) {
+		
+	}
+	
+	public void attribuerRenfortAleatoire(String idJoueur) {
+		
+	}
+}
