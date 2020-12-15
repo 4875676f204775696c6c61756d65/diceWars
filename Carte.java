db8c9bb8012b304f333003f0010ec5a8a93a6ceb
Baudouindlv <71937583+Baudouindlv@users.noreply.github.com>
2020-12-15 20:42:35 +0100

    Update Carte.java

---
+++ Carte.java
@@ -0,0 +1,17 @@
+package projet;
+
+public class Carte{
+	private Territoire[][] listeTerritoire;
+	
+	public Carte() {
+		//constructeur aleatoire
+	}
+	
+	public Carte(String path) {
+		//constructeur a partir d'un fichier csv
+	}
+	
+	
+	
+	
+}
